007665720abc1467c9a1d7a0e153f2087380729c
prince-nda <p.ndahiro1@alustudent.com>
2026-03-26 15:36:19 +0200

    initial changes

--- App/extensions.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_jwt_extended import JWTManager
-
-# Initialize the extensions
-
-db = SQLAlchemy()
-bcrypt = Bcrypt()
-jwt = JWTManager()
